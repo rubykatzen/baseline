f80f7ff9841afdbde87ce1d9402ab2556eed233a
Yuriy Kirillov <ykirillov@icloud.com>
2026-06-15 16:53:33 +0200

    Revert "fix: address review comments on Ruby linters"
    This reverts commit a18de498c40aeb7f2a75874533fa5be7474d7c5d.

--- hooks/rubocop.sh
+++ hooks/rubocop.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 BASELINE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-bundle exec rubocop --config "$BASELINE_DIR/configs/rubocop.yml" "$@"
+
+cat > .rubocop.yml << RUBOCOP
+inherit_from:
+  - $BASELINE_DIR/configs/rubocop.yml
+  - .rubocop_todo*.yml
+RUBOCOP
+
+trap "rm -f .rubocop.yml" EXIT
+bundle exec rubocop "$@"
