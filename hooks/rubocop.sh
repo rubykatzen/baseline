c74d1e103234c27b6bfed12121014a2c00f36c5e
Yuriy Kirillov <ykirillov@icloud.com>
2026-06-16 02:24:57 +0200

    rubocop: use find + xargs -0 instead of unquoted variable

--- hooks/rubocop.sh
+++ hooks/rubocop.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 BASELINE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-# When called with file args (pre-commit), use those.
-# Otherwise find all Ruby files, explicitly excluding vendor/node_modules.
-if [ $# -gt 0 ]; then
-  RUBY_FILES="$*"
-else
-  RUBY_FILES=$(find . \
+ruby_files() {
+  find . \
     \( -path ./.git -o -path ./vendor -o -path ./node_modules \) -prune -o \
     -type f \
     \( -name '*.rb' -o -name '*.rake' -o -name '*.gemspec' -o -name 'Gemfile' -o -name 'Rakefile' -o -name 'config.ru' \) \
-    -print)
-fi
+    "$@"
+}
 
-if [ -z "$RUBY_FILES" ]; then
+if [ $# -eq 0 ] && ! ruby_files -print -quit | grep -q .; then
   printf '%s\n' 'No Ruby files found; skipping rubocop.'
   exit 0
 fi
 
 RUBOCOP_CONFIG="$(mktemp)"
-
 trap 'rm -f "$RUBOCOP_CONFIG"' EXIT
 
 {
@@ -32,10 +27,16 @@ trap 'rm -f "$RUBOCOP_CONFIG"' EXIT
   done
 } > "$RUBOCOP_CONFIG"
 
-if [ -f Gemfile ]; then
-  # shellcheck disable=SC2086
-  bundle exec rubocop --config "$RUBOCOP_CONFIG" $RUBY_FILES
+if [ $# -gt 0 ]; then
+  if [ -f Gemfile ]; then
+    bundle exec rubocop --config "$RUBOCOP_CONFIG" "$@"
+  else
+    rubocop --config "$RUBOCOP_CONFIG" "$@"
+  fi
 else
-  # shellcheck disable=SC2086
-  rubocop --config "$RUBOCOP_CONFIG" $RUBY_FILES
+  if [ -f Gemfile ]; then
+    ruby_files -print0 | xargs -0 bundle exec rubocop --config "$RUBOCOP_CONFIG"
+  else
+    ruby_files -print0 | xargs -0 rubocop --config "$RUBOCOP_CONFIG"
+  fi
 fi
