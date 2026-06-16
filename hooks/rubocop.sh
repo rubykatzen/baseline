bb0d609d0bc6b4d608ab4c22ad91ac5fa7500715
Yuriy Kirillov <ykirillov@icloud.com>
2026-06-16 02:21:11 +0200

    rubocop: use find to collect files instead of relying on AllCops/Exclude

--- hooks/rubocop.sh
+++ hooks/rubocop.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 BASELINE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-has_rubocop_targets() {
-  find . \
+# When called with file args (pre-commit), use those.
+# Otherwise find all Ruby files, explicitly excluding vendor/node_modules.
+if [ $# -gt 0 ]; then
+  RUBY_FILES="$*"
+else
+  RUBY_FILES=$(find . \
     \( -path ./.git -o -path ./vendor -o -path ./node_modules \) -prune -o \
     -type f \
     \( -name '*.rb' -o -name '*.rake' -o -name '*.gemspec' -o -name 'Gemfile' -o -name 'Rakefile' -o -name 'config.ru' \) \
-    -print -quit | grep -q .
-}
+    -print)
+fi
 
-if ! has_rubocop_targets; then
+if [ -z "$RUBY_FILES" ]; then
   printf '%s\n' 'No Ruby files found; skipping rubocop.'
   exit 0
 fi
@@ -27,8 +31,11 @@ trap 'rm -f "$RUBOCOP_CONFIG"' EXIT
     printf '  - %s\n' "$PWD/$todo"
   done
 } > "$RUBOCOP_CONFIG"
+
 if [ -f Gemfile ]; then
-  bundle exec rubocop --config "$RUBOCOP_CONFIG" "$@"
+  # shellcheck disable=SC2086
+  bundle exec rubocop --config "$RUBOCOP_CONFIG" $RUBY_FILES
 else
-  rubocop --config "$RUBOCOP_CONFIG" "$@"
+  # shellcheck disable=SC2086
+  rubocop --config "$RUBOCOP_CONFIG" $RUBY_FILES
 fi
